2813208cf70929e12a9b5683d963825057a350ce
Платон Жемеров <zhemerov.p@mail.ru>
2022-11-28 17:29:51 +0300

    small fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -47,6 +47,8 @@ class CommentViewSet(viewsets.ModelViewSet):
 class FollowViewSet(mixins.CreateModelMixin,
                     mixins.ListModelMixin,
                     viewsets.GenericViewSet):
+    # Если убрать 1 queryset, тесты вообще перестают видеть queryset'ы
+    queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     pagination_class = pagination.LimitOffsetPagination
     permission_classes = (permissions.IsAuthenticated,)
